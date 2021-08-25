ce321507b3696ae09b394b25f7112092d647d54b
Bernardo Castro <bernardoc544@gmail.com>
2021-08-25 15:44:40 -0400

    Configuring Firebase

---
+++ src/firebaseConnection.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyBiKPBoOjtKCR214a_ypmeRz6piAynbRi8",
+  
+    authDomain: "curso-20096.firebaseapp.com",
+  
+    projectId: "curso-20096",
+  
+    storageBucket: "curso-20096.appspot.com",
+  
+    messagingSenderId: "475247050718",
+  
+    appId: "1:475247050718:web:392bb7c83a61d6217ca74f",
+  
+    measurementId: "G-EH5XVNRNCB"
+  
+  };
+  
+  if(!firebase.apps.length){
+    const app = initializeApp(firebaseConfig);
+  }
+  
+export default firebase;
+  
+  
+  
