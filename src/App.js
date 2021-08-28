53016c57b5d443388ae1dc59ddbdefa9fa9dd39c
Bernardo Castro <bernardoc544@gmail.com>
2021-08-28 12:04:16 -0400

    Lists

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './style.css'
 function App() {
   const [titulo, setTitulo] = useState('')
   const [autor, setAutor] = useState('')
+  const [dados, setDados] = useState([])
 
   async function handleAdd(){
     await firebase.firestore().collection('posts')
@@ -26,14 +27,32 @@ function App() {
     })
   }
   async function handleRead(){
-    await firebase.firestore().collection('posts')
-    .doc('JYGFjeCOLD15qqpaSk1L')
+    /*await firebase.firestore().collection('posts')
+    .doc('123')
     .get()
     .then((snapshot)=>{
       setTitulo(snapshot.data().titulo)
       setAutor(snapshot.data().autor)
     })
     .catch(()=>{
+      console.log('ERRO')
+    })*/
+
+    await firebase.firestore().collection('posts')
+    .get()
+    .then((snapshot)=> {
+      let lista = []
+      snapshot.forEach((doc)=>{
+        lista.push({
+          id: doc.id,
+          titulo: doc.data().titulo,
+          autor: doc.data().autor
+        })
+      })
+
+      setDados(lista)
+    })
+    .catch(() => {
       console.log('ERRO')
     })
   }
@@ -49,7 +68,18 @@ function App() {
         <textarea type="text" value={autor} onChange={(e) => setAutor(e.target.value)}/>
 
         <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={handleRead}>Buscar</button>
+        <button onClick={handleRead}>Buscar</button><br/>
+
+        <ul>
+          {dados.map((post)=>{
+            return(
+              <li key={post.id}>
+                <span>Titulo: {post.titulo}</span><br/>
+                <span>Autor: {post.autor}</span><br/><br/>
+              </li>
+            )
+          })}
+        </ul>
       </div>
       
     </div>
