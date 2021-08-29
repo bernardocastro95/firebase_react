9b7ce765512aba746d20d281abf68d01d3cec221
Bernardo Castro <bernardoc544@gmail.com>
2021-08-29 12:14:11 -0400

    Edit function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import firebase from "./firebaseConnection";
 import {useState, useEffect} from 'react'
 import './style.css'
 function App() {
+  const [idPost, setIdPost] = useState('')
   const [titulo, setTitulo] = useState('')
   const [autor, setAutor] = useState('')
   const [dados, setDados] = useState([])
@@ -77,11 +78,32 @@ function App() {
       console.log('ERRO')
     })
   }
+
+  async function handleEdit(){
+    await firebase.firestore().collection('posts')
+    .doc(idPost)
+    .update({
+      titulo: titulo,
+      autor: autor
+    })
+    .then(()=>{
+      console.log('DADOS ATUALIZADOS COM SUCESSO')
+      setIdPost('')
+      setTitulo('')
+      setAutor('')
+    })
+    .catch(()=>{
+      console.log('FALHA AO ATUALIZAR')
+    })
+  }
   return (
     <div>
       <h1>React + Firebase</h1><br/>
 
       <div className="container">
+
+        <label>ID:</label>
+        <input type="text" value={idPost} onChange={(e) => setIdPost(e.target.value)}/>
         <label>Título</label>
         <textarea type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)}/>
 
@@ -89,12 +111,14 @@ function App() {
         <textarea type="text" value={autor} onChange={(e) => setAutor(e.target.value)}/>
 
         <button onClick={handleAdd}>Cadastrar</button>
-        <button onClick={handleRead}>Buscar</button><br/>
+        <button onClick={handleRead}>Buscar</button>
+        <button onClick={handleEdit}>Editar</button>
 
         <ul>
           {dados.map((post)=>{
             return(
               <li key={post.id}>
+                <span>Id: {post.id}</span><br/>
                 <span>Titulo: {post.titulo}</span><br/>
                 <span>Autor: {post.autor}</span><br/><br/>
               </li>
